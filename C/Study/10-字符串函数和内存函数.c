ddd88b8e4be89659018abd83696395ff6ee3321b
Orion-Zhang <KilluaAoki@gmail.com>
2022-08-13 12:47:25 +0800

    Update 10-字符串函数和内存函数.c
    1. Add annotation level.
    2. Supplementary knowledge.
    3. Unified format.
    4. Rich annotations.
    5. Fix bugs.
    6. Optimization function.

--- C/Study/10-字符串函数和内存函数.c
+++ C/Study/10-字符串函数和内存函数.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
 /*
 	字符串函数和内存函数
@@ -15,7 +15,7 @@
 			a.内存操作函数："memcpy"函数、"memmove"函数、"memset"函数、"memcmp"函数。
 */
 
-/*
+/**
 	"strlen"函数：获取字符串长度。
 		1.头文件："string.h"。
 		2.函数声明：size_t strlen(const char* str);。
@@ -46,6 +46,7 @@
 //	}
 //	return count;
 //}
+//
 //size_t My_Strlen_Recursion(const char* str)
 //{
 //	if (!*str)
@@ -54,12 +55,14 @@
 //	}
 //	return 1 + My_Strlen_Recursion(str + 1);
 //}
+//
 //size_t My_Strlen_Pointer(const char* str)
 //{
 //	const char* tmp = str;
 //	while (*tmp++);
 //	return tmp - str - 1;
 //}
+//
 //int main()
 //{
 //	char* str = "KilluaAoki";
@@ -83,17 +86,18 @@
 //	return 0;
 //}
 
-/*
-	"strcpy"函数：复制字符串(包含"\0")。
+/**
+	"strcpy"函数：复制字符串。
 		1.头文件："string.h"。
 		2.函数声明：char* strcpy(char* destination, const char* source);。
 			a.函数头"char*"表示此函数的返回值是字符指针类型，在此函数中表示返回"destination"指针所指向的字符串的起始地址。
 			b.函数参数"char* destination"：将字符指针作为参数；在此函数中意为指向要复制内容的目标数组首元素的指针(数组名)。
-			c.函数参数"const char* source"：将字符指针作为参数，并转换为由"const"修饰的"char*"类型(const char*)；在此函数中意为指向作为复制来源的字符串的指针(此字符串需包含"\0")。。
+			c.函数参数"const char* source"：将字符指针作为参数，并转换为由"const"修饰的"char*"类型(const char*)；在此函数中意为指向作为复制来源的字符串的指针(此字符串需包含"\0")。
 		3.未定义情况
-			a.若目标空间数组长度不足，则行为未定义。
+			a.若目标空间数组长度不足(即空间过小)，无法容纳作为复制来源的字符串，则行为未定义。
 			b.若字符串重叠，即"destination"在内存中与"source"重叠，则行为未定义。
 			c.若"destination"不是指向字符数组元素的指针或"source"不是指向包含"\0"的字符串的指针，则行为未定义。
+			d.复制"source"所指向的空终止字节字符串，包含空终止符，到首元素为"destination"所指的字符数组。
 */
 
 ////使用"strcpy"函数示例：于"VS2022"中无法使用"strcpy"，若要使用需使用"_CRT_SECURE_NO_WARNINGS"，此处以"strcpy_s"进行演示复制字符串操作，但其返回值和参数与"strcpy"不相同，"strcpy_s"具体使用方法请查看文档。
@@ -110,9 +114,10 @@
 //char* My_Strcpy(char* destination, const char* source)
 //{
 //	char* tmp = destination;
-//	while (*tmp++ = *source++);
+//	while ((*tmp++ = *source++));
 //	return destination;
 //}
+//
 //int main()
 //{
 //	char arr1[15] = "XXXXXXXXXX";
@@ -121,13 +126,13 @@
 //	return 0;
 //}
 
-/*
+/**
 	"strcat"函数：连接字符串，即将源字符串的副本附加到目标字符串。
 		1.头文件："string.h"。
 		2.函数声明：char* strcat(char* destination, const char* source);。
 			a.函数头"char*"表示此函数的返回值是字符指针类型，在此函数中表示返回"destination"指针所指向的字符串的起始地址。
 			b.函数参数"char* destination"：将字符指针作为参数；在此函数中意为指向要附加到的以"\0"字符结尾的字符串的指针。
-			c.函数参数"const char* source"：将字符指针作为参数，并转换为由"const"修饰的"char*"类型(const char*)；在此函数中意为指向作为追加来源的字符串的指针(此字符串需包含"\0")。。
+			c.函数参数"const char* source"：将字符指针作为参数，并转换为由"const"修饰的"char*"类型(const char*)；在此函数中意为指向作为追加来源的字符串的指针(此字符串需包含"\0")。
 		3.未定义情况
 			a.若目标空间数组长度不足以存放对于"source"和"destination"的内容以及空终止符，则行为未定义。
 			b.若字符串重叠，即"destination"在内存中与"source"重叠，则行为未定义。
@@ -150,13 +155,12 @@
 //char* My_Strcat(char* destination, const char* source)
 //{
 //	char* tmp = destination;
-//	while (*tmp)
-//	{
-//		tmp++;
-//	}
-//	while (*tmp++ = *source++);
+//	while (*tmp++);
+//	--tmp;
+//	while ((*tmp++ = *source++));
 //	return destination;
 //}
+//
 //int main()
 //{
 //	char arr1[20];
@@ -166,7 +170,7 @@
 //	return 0;
 //}
 
-/*
+/**
 	"strcmp"函数：以字典序比较二个字符串的字符，此函数从两个字符串的第一个字符开始比较，如果它们彼此相等，则继续向后比较，直到两字符的字符不同或到达终止空字符。
 		1.头文件："string.h"。
 		2.函数声明：int strcmp(const char* str1, const char* str2);。
@@ -189,7 +193,7 @@
 //	{
 //		printf("请输入密码>>");
 //		scanf_s("%s", Get, 20);
-//		if (strcmp(Key,Get) == 0)
+//		if (strcmp(Key, Get) == 0)
 //		{
 //			printf("\n密码输入正确！\n");
 //			break;
@@ -213,13 +217,11 @@
 ////模拟实现"strcmp"函数
 //int My_Strcmp(const char* str1, const char* str2)
 //{
-//	while (*str1 != '\0' && (*(unsigned char*)str1 - *(unsigned char*)str2 == 0))
-//	{
-//		str1++;
-//		str2++;
-//	}
-//	return *(unsigned char*)str1 - *(unsigned char*)str2;
+//	int ret = 0;
+//	while (((ret = ((int)(*((unsigned char*)str1))) - *((unsigned char*)str2++)) == 0) && *str1++);
+//	return ret;
 //}
+//
 //int main()
 //{
 //	char Key[] = "KilluaAoki";
@@ -229,7 +231,7 @@
 //	{
 //		printf("请输入密码>>");
 //		scanf_s("%s", Get, 20);
-//		if (My_Strcmp(Key,Get) == 0)
+//		if (My_Strcmp(Key, Get) == 0)
 //		{
 //			printf("\n密码输入正确！\n");
 //			break;
@@ -250,7 +252,7 @@
 //	return 0;
 //}
 
-/*
+/**
 	"strncpy"函数：从字符串中复制字符，并可指定复制的字符个数。
 		1.头文件："string.h"。
 		2.函数声明：char *strncpy(char* destination, const char* source, size_t count);。
@@ -272,28 +274,25 @@
 //char* My_Strncpy(char* destination, const char* source, size_t count)
 //{
 //	char* start = destination;
-//	while (count && (*destination++ = *source++) != '\0')
+//	while (count--)
 //	{
-//		count--;
-//	}
-//	if (count)
-//	{
-//		while (--count)
+//		if ((*destination++ = *source))
 //		{
-//			*destination++ = '\0';
+//			++source;
 //		}
 //	}
 //	return start;
 //}
+//
 //int main()
 //{
-//	char arr1[20] = "XXXXXX Aoki";
+//	char arr1[20] = "XXXXXXAoki";
 //	char arr2[] = "Killua";
-//	printf("%s\n", My_Strncpy(arr1, arr2, 6));
+//	printf("%s\n", My_Strncpy(arr1, arr2, 10));
 //	return 0;
 //}
 
-/*
+/**
 	"strncat"函数：从字符串追加字符，并可指定追加的字符个数。
 		1.头文件："string.h"。
 		2.函数声明：char *strncat(char* destination, const char* source, size_t count);。
@@ -304,7 +303,7 @@
 		3.函数说明
 			a.追加来自"source"所指向的字符数组的至多"count"个字符到"destination"所指向的空终止字节字符串的末尾，若找到空字符则停止。
 			b."source"指向的首元素将替换位于"destination"末尾的空终止符。
-			c.始终追加终止空字符到末尾，故函数可写入的最大字节数是"const+1"。
+			c.始终追加终止空字符到末尾，故函数可写入的最大字节数是"const + 1"。
 			d.如果"count"大于"source"的长度，则会使用"source"的长度代替"count"。
 		4.未定义情况
 			a.若目标数组没有足够的空间容纳对于"destination"和"source"的"count"个字符以及终止空字符，则行为未定义。
@@ -328,23 +327,21 @@
 //{
 //	char* tmp = destination;
 //	while (*tmp++);
-//	tmp--;
-//	while (count--)
+//	--tmp;
+//	while (count-- && (*tmp = *source++))
 //	{
-//		if ((*tmp++ = *source++) == '\0')
-//		{
-//			return destination;
-//		}
+//		++tmp;
 //	}
 //	*tmp = '\0';
 //	return destination;
 //}
+//
 //int main()
 //{
 //	char arr1[20];
 //	char arr2[] = "Aoki";
 //	strcpy_s(arr1, 20, "Killua");
-//	printf("%s\n", My_Strncat(arr1, arr2, 5));
+//	printf("%s\n", My_Strncat(arr1, arr2, 10));
 //	return 0;
 //}
 
@@ -377,7 +374,7 @@
 //	{
 //		printf("请输入密码>>");
 //		scanf_s("%s", Get, 20);
-//		if (strncmp(Key,Get,20) == 0)
+//		if (strncmp(Key, Get,20) == 0)
 //		{
 //			printf("\n密码输入正确！\n");
 //			break;
@@ -401,13 +398,11 @@
 ////模拟实现"strncmp"函数
 //int My_Strncmp(const char *str1, const char *str2, size_t count)
 //{
-//    while (count-- && (*(const unsigned char*)str1 - *(const unsigned char*)str2) == 0 && *str1 != '\0')
-//    {
-//        str1++;
-//        str2++;
-//    }
-//    return *(const unsigned char*)str1 - *(const unsigned char*)str2;
+//	int ret = 0;
+//	while (count-- && ((ret = ((int)(*((unsigned char*)str1))) - *((unsigned char*)str2++)) == 0) && *str1++);
+//	return ret;
 //}
+//
 //int main()
 //{
 //	char Key[] = "KilluaAoki";
@@ -417,7 +412,7 @@
 //	{
 //		printf("请输入密码>>");
 //		scanf_s("%s", Get, 20);
-//		if (My_Strncmp(Key,Get,20) == 0)
+//		if (My_Strncmp(Key, Get,20) == 0)
 //		{
 //			printf("\n密码输入正确！\n");
 //			break;
@@ -438,9 +433,9 @@
 //	return 0;
 //}
 
-/*
-    "strstr"函数：查找字符串，在主串中定位字串。
-        1.头文件："string.h"。
+/**
+	"strstr"函数：查找字符串，在主串中定位字串。
+		1.头文件："string.h"。
 		2.函数声明：char *strstr(const char* str, const char* substr);。
 			a.函数头"char*"表示此函数的返回值是字符指针类型；在此函数中表示返回指向"substr"中指定的整个字符序列中第一次出现在"str"中的指针；如果该序列不存在于"str"中，则返回空指针；若"substr"指向空字符串，则返回"str"。
 			b.函数参数"const char* str"：将字符指针作为参数，并转换为由"const"修饰的"char*"类型(const char*)；在此函数中意为指向要检验的空终止字节字符串的指针(此字符串需包含"\0")。
@@ -454,45 +449,49 @@
 ////使用"strstr"函数示例
 //int main()
 //{
-//    char arr1[]="XXXXXXAoki";
-//    char arr2[]="Aoki";
-//    printf("%s\n", strstr(arr1,arr2));
-//    return 0;
+//	char arr1[]="XXXXXXAoki";
+//	char arr2[]="Aoki";
+//	printf("%s\n", strstr(arr1,arr2));
+//	return 0;
 //}
 
 ////模拟实现"strstr"函数
 //char* My_Strstr(const char* str, const char* substr)
 //{
-//	if (*substr == '\0')
+//	const char* str1 = str;
+//	const char* str2 = substr;
+//	do
 //	{
-//		return (char*)str;
-//	}
-//	while (*str)
-//	{
-//		const char* tmp1 = str;
-//		const char* tmp2 = substr;
-//		while ((*tmp1 == *tmp2) && *tmp1)
-//		{
-//			tmp1++;
-//			tmp2++;
-//		}
-//		if (*tmp2 == '\0')
+//		if (*str2 == '\0')
 //		{
 //			return (char*)str;
 //		}
-//		tmp1 = ++str;
-//	}
-//	return NULL;
+//		if (*str1 == *str2)
+//		{
+//			str1++;
+//			str2++;
+//		}
+//		else
+//		{
+//			str2 = substr;
+//			if (*str1 == '\0')
+//			{
+//				return NULL;
+//			}
+//			str1 = ++str;
+//		}
+//	} while (1);
 //}
+//
 //int main()
 //{
-//    char arr1[]="XXXXXXAoki";
-//    char arr2[]="Aoki";
-//    printf("%s\n", My_Strstr(arr1,arr2));
-//    return 0;
+//	char arr1[]="XXXXXXAoki";
+//	char arr2[]="Aoki";
+//	printf("%s\n", My_Strstr(arr1,arr2));
+//	return 0;
 //}
 
-/*
+/**
 	"strtok"函数：以标记拆分字符串。
 		1.头文件："string.h"。
 		2.函数声明：char* strtok(char* str, const char* delimiters);。
@@ -526,7 +525,7 @@
 //	return 0;
 //}
 
-/*
+/**
 	"strerror"函数：获取指向错误消息字符串的指针。
 		1.头文件："string.h"(包含"strerror"函数)、"errno.h"(包含全局整型变量"errno")。
 		2.函数声明：char* strerror(int errnum);。
@@ -544,7 +543,7 @@
 //	////使用"strerror"函数打开无效文件并打印错误信息
 //	//FILE* ptr = NULL;
 //	//fopen_s(&ptr, "test.txt", "r");//以只读方式打开"test.txt"文件，打开文件成功会将有效指针写入"ptr"，打开文件失败会将空指针写入"ptr"。
-//	//char buffer[20] = { 0 };
+//	//char buffer[50] = { 0 };
 //	//if (ptr == NULL)
 //	//{
 //	//	strerror_s(buffer, 20, errno);
@@ -555,7 +554,7 @@
 //	////使用"perror"函数打开无效文件并打印错误信息
 //	//FILE* ptr = NULL;
 //	//fopen_s(&ptr, "test.txt", "r");
-//	//char buffer[20] = { 0 };
+//	//char buffer[50] = { 0 };
 //	//if (ptr == NULL)
 //	//{
 //	//	perror("fopen_s->错误信息");//直接拿取错误码并转换为错误信息，并打印错误信息(包括自定义信息)。
@@ -569,7 +568,7 @@
 //	return 0;
 //}
 
-/*
+/**
 	字符操作函数
 		1.字符分类函数
 			a."iscntrl"：任何控制字符。
@@ -588,7 +587,7 @@
 			b."toupper"：将小写字母转换为大写字母。
 */
 
-/*
+/***
 	字符分类函数
 		1.头文件："ctype.h"。
 		2.函数声明除函数名外，其他参数相同，函数头类型都为"int"，函数参数类型都为"int"(字符类型转换为"int"或"EOF")。
@@ -612,7 +611,7 @@
 //	return 0;
 //}
 
-/*
+/***
 	字符转换函数
 		1.头文件："ctype.h"。
 		2.函数声明除函数名外，其他参数相同，函数头类型都为"int"，函数参数类型都为"int"(字符类型转换为"int"或"EOF")。
@@ -641,8 +640,8 @@
 //	return 0;
 //}
 
-/*
-	"memcpy"函数：复制内存块。(C标准库要求此函数只需实现复制不重叠内存块即可，但于"VS2022"中"memcpy"函数同时实现了可复制重叠内存块功能)
+/**
+	"memcpy"函数：复制内存块。(C标准库要求此函数只需实现复制不重叠内存块即可，但"VS2022"编译器中"memcpy"函数同时实现了可复制重叠内存块功能)
 		1.头文件："string.h"。
 		2.函数声明：void* memcpy(void* destination, const void* source, size_t num);。
 			a.函数头"void*"表示此函数的返回值是无类型指针，可返回任意类型的指针变量；在此函数中意为返回"destination"指针所指向的起始地址。
@@ -680,23 +679,15 @@
 ////模拟实现"memcpy"函数
 //void* My_Memcpy(void* destination, const void* source, size_t num)
 //{
-//	////方法一
-//	//unsigned char* str1 = (unsigned char*)destination;
-//	//const unsigned char* str2 = (const unsigned char*)source;
-//	//while (num--)
-//	//{
-//	//	*str1++ = *str2++;
-//	//}
-//
-//	//方法二
-//	void* tmp = destination;
+//	unsigned char* str1 = destination;
+//	const unsigned char* str2 = source;
 //	while (num--)
 //	{
-//		*((unsigned char*)(tmp = (unsigned char*)tmp + 1) - 1) = *((const unsigned char*)(source = (const unsigned char*)source + 1) - 1);
+//		*str1++ = *str2++;
 //	}
-//
 //	return destination;
 //}
+//
 //int main()
 //{
 //	int arr1[10] = { 0 };
@@ -709,7 +700,7 @@
 //	return 0;
 //}
 
-/*
+/**
 	"memmove"函数：移动内存块。
 		1.头文件："string.h"。
 		2.函数声明：除函数名外其余于"memcpy"函数相同。
@@ -718,10 +709,10 @@
 			b.指针指向的对象的底层类型与此函数无关，结果是数据的二进制副本。
 			c.该函数不检查源中的任何终止空字符，它将直接精确复制"num"个字节数据。
 			d.可用于设置分配函数所获得对象的有效类型。
-			e.区别于"memcpy"函数，"memmove"允许目标空间和源空间重叠，但不如"memcpy"高效。
+			e.区别于"memcpy"函数，"memmove"函数允许目标空间和源空间重叠，但不如"memcpy"函数高效。
 			f.尽管说明了"如同"使用临时缓冲区，但此函数的实际实现不会带来二次复制或额外内存的开销。
 			g.在严格别名禁止检验同一内存作为两种不同类型的值的情况下，可用"memmove"函数转换值。
-			h.若目标在源之前开始，则从缓冲区开始正向复制，否则从末尾反向复制，完全无重叠时回落到更高效的"memcpy"。
+			h.若目标在源之前开始，则从缓冲区开始正向复制，否则从末尾反向复制，完全无重叠时回落到更高效的"memcpy"函数。
 		4.未定义情况
 			a.若访问发生在"destination"数组结尾后，则行为未定义。
 			b.若"destination"或"source"为非法或空指针，则行为未定义。
@@ -743,29 +734,31 @@
 ////模拟实现"memmove"函数
 //void* My_Memmove(void* destination, const void* source, size_t num)
 //{
-//	void* ptr = destination;
-//	
-//	if (ptr < source)
+//	unsigned char* ptr1 = (unsigned char*)destination;
+//	const unsigned char* ptr2 = (const unsigned char*)source;
+//
+//	if (ptr1 > ptr2)
 //	{
 //		while (num--)
 //		{
-//			*((unsigned char*)(ptr = (unsigned char*)ptr + 1) - 1) = *((const unsigned char*)(source = (const unsigned char*)source + 1) - 1);
+//			ptr1[num] = ptr2[num];
 //		}
 //	}
 //	else
 //	{
 //		while (num--)
 //		{
-//			*((unsigned char*)ptr + num) = *((unsigned char*)source + num);
+//			*ptr1++ = *ptr2++;
 //		}
 //	}
 //
 //	return destination;
 //}
+//
 //int main()
 //{
 //	int arr[10] = { 5,6,7,8,9,0,1,2,3,4 };
-//	My_Memmove(arr, arr + 5, sizeof(arr[0]) * 5);
+//	My_Memmove(arr + 1, arr + 4, sizeof(arr[0]) * 4);
 //	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
 //	{
 //		printf("%d ", arr[i]);
@@ -773,7 +766,7 @@
 //	return 0;
 //}
 
-/*
+/**
 	"memcmp"函数：以字典序比较两个内存块，并可指定比较的字节大小。
 		1.头文件："string.h"。
 		2.函数声明：int memcmp(const void* ptr1, const void* ptr2, size_t num);。
@@ -793,7 +786,7 @@
 			b.若"ptr1"或"ptr2"为空指针，则行为未定义。
 */
 
-////使用"memcmp"函数示例
+////模拟实现"memcmp"函数示例
 //int main()
 //{
 //	char buffer1[] = "KilluaAoki";
@@ -817,7 +810,40 @@
 //	return 0;
 //}
 
-/*
+////模拟实现"memcmp"函数
+//int My_Memcmp(const void* ptr1, const void* ptr2, size_t num)
+//{
+//	int ret = 0;
+//	const unsigned char* p1 = (const unsigned char*)ptr1;
+//	const unsigned char* p2 = (const unsigned char*)ptr2;
+//	while (num-- && ((ret = ((int)(*p1++)) - *p2++) == 0));
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char buffer1[] = "KilluaAoki";
+//	char buffer2[] = "KilluaAoki";
+//
+//	int n = memcmp(buffer1, buffer2, sizeof(buffer1));
+//
+//	if (n > 0)
+//	{
+//		printf("'%s' is greater than '%s'.\n", buffer1, buffer2);
+//	}
+//	else if (n < 0)
+//	{
+//		printf("'%s' is less than '%s'.\n", buffer1, buffer2);
+//	}
+//	else
+//	{
+//		printf("'%s' is the same as '%s'.\n", buffer1, buffer2);
+//	}
+//
+//	return 0;
+//}
+
+/**
 	"memset"函数：填充内存块。
 		1.头文件："string.h"。
 		2.函数声明：void* memset(void* ptr, int value, size_t num);。
@@ -825,7 +851,9 @@
 			b.函数参数"void* ptr"：将无类型指针作为参数，可接收任意类型的指针变量；在此函数中意为指向要填充的内存块的指针(类型转换为void*)。
 			c.函数参数"int value"：将整型类型作为参数；在此函数中意为要设置的值，该值作为整型传递，但该函数会将该值转换为无符号字符类型填充内存块。
 			d.函数参数"size_t num"：将无符号整型类型作为参数；在此函数中意为设置要填充的字节数。
-		3.函数说明：将值"value"(类型转换为"unsigned char")复制到"ptr"所指向对象的最前面"num"个字节中。
+		3.函数说明
+			a.将值"value"(类型转换为"unsigned char")复制到"ptr"所指向对象的最前面"num"个字节中。
+			b.会将输入范围中的每个字节都填充为值"value"，并且是以值"value"的低八位进行赋值。
 		4.未定义情况
 			a.若在"ptr"所指向的对象结尾后出现访问，则行为未定义。
 			b.若"ptr"为空指针，则行为未定义。
@@ -835,7 +863,44 @@
 //int main()
 //{
 //	char str[] = "KilluaAoki";
-//	memset(str+1, '*', 8);
+//	memset(str + 1, '*', sizeof(char) * 8);
 //	puts(str);
+//	return 0;
+//}
+
+////模拟实现"memset"函数
+//void* My_Memset(void* ptr, int value, size_t num)
+//{
+//	unsigned char* tmp = (unsigned char*)ptr;
+//	while (num--)
+//	{
+//		*tmp++ = (unsigned char)value;
+//	}
+//	return ptr;
+//}
+//
+//void PrintArray(int arr[], size_t size)
+//{
+//	for (size_t i = 0; i < size; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int arr[10];
+//
+//	size_t size = sizeof(arr) / sizeof(arr[0]);
+//	
+//	My_Memset(arr, 0, sizeof(arr));//只将"value"二进制的低八位赋值给所输范围的每个字节。
+//	
+//	PrintArray(arr, size);
+//
+//	My_Memset(arr, 1, sizeof(arr));//会将输入范围中的每个字节都填充为值"value"。
+//
+//	PrintArray(arr, size);
+//	
 //	return 0;
 //}
