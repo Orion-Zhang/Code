39a7949ce32f806c00b86a94cf600585c71c502e
Orion-Zhang <KilluaAoki@gmail.com>
2022-09-08 18:28:31 +0800

    Update Exercise.c

--- Data Structure/Exercise/Exercise.c
+++ Data Structure/Exercise/Exercise.c
@@ -278,3 +278,16 @@
 //	}
 //	return prev;
 //}
+
+////链表的中间结点：https://leetcode.cn/problems/middle-of-the-linked-list/。
+//struct ListNode* middleNode(struct ListNode* head)
+//{
+//	struct ListNode* fast, * slow;
+//	fast = slow = head;
+//	while (fast && fast->next)
+//	{
+//		slow = slow->next;
+//		fast = fast->next->next;
+//	}
+//	return slow;
+//}
