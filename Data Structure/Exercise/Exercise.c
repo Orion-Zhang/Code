82522eba772c775fec3c678dc8d0d8c02101d9f4
Orion-Zhang <KilluaAoki@gmail.com>
2022-09-12 22:48:43 +0800

    Update Exercise.c

--- Data Structure/Exercise/Exercise.c
+++ Data Structure/Exercise/Exercise.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 ////轮转数组：https://leetcode-cn.com/problems/rotate-array/。(使用额外数组的方法)
 //void rotate(int* nums, int numsSize, int k)
 //{
@@ -411,4 +414,238 @@
 //		n1 = n2;
 //	}
 //	return res;
+//}
+
+////相交链表：给定两个可能有环也可能无环的单链表，头节点为"head1"和"head2"。请实现一个函数，如果两个链表相交，请返回链表相交的第一个节点，如果不相交，返回"NULL"。
+////要求：
+////	1. 如果两个链表的长度之和为"N"，时间复杂度请达到"O(N)"，额外空间复杂度请达到"O(1)"。
+//typedef int SLLDataType;
+//
+//typedef struct SinglyLinkedListNode
+//{
+//	SLLDataType data;
+//	struct SinglyLinkedListNode* next;
+//} SLLNode;
+//
+//SLLNode* Create_SinglyLinkedListNode(SLLDataType data)
+//{
+//	SLLNode* newNode = (SLLNode*)malloc(sizeof(SLLNode));
+//	if (newNode == NULL)
+//	{
+//		perror("Create_SinglyLinkedListNode");
+//		exit(EXIT_FAILURE);
+//	}
+//	newNode->data = data;
+//	newNode->next = NULL;
+//	return newNode;
+//}
+//
+//SLLNode* GetLoopNode(SLLNode* head)
+//{
+//	if (head == NULL || head->next == NULL || head->next->next == NULL)
+//	{
+//		return NULL;
+//	}
+//	SLLNode* slow = head->next;
+//	SLLNode* fast = head->next->next;
+//	while (slow != fast)
+//	{
+//		if (fast->next == NULL || fast->next->next == NULL)
+//		{
+//			return NULL;
+//		}
+//		slow = slow->next;
+//		fast = fast->next->next;
+//	}
+//	fast = head;
+//	while (slow != fast)
+//	{
+//		slow = slow->next;
+//		fast = fast->next;
+//	}
+//	return slow;
+//}
+//
+//SLLNode* NoLoop(SLLNode* head1, SLLNode* head2)
+//{
+//	if (head1 == NULL || head2 == NULL)
+//	{
+//		return NULL;
+//	}
+//	SLLNode* cur1 = head1;
+//	SLLNode* cur2 = head2;
+//	int n = 0;
+//	while (cur1->next != NULL)
+//	{
+//		++n;
+//		cur1 = cur1->next;
+//	}
+//	while (cur2->next != NULL)
+//	{
+//		--n;
+//		cur2 = cur2->next;
+//	}
+//	if (cur1 != cur2)
+//	{
+//		return NULL;
+//	}
+//	cur1 = n > 0 ? head1 : head2;
+//	cur2 = cur1 == head1 ? head2 : head1;
+//	n = abs(n);
+//	while (n--)
+//	{
+//		cur1 = cur1->next;
+//	}
+//	while (cur1 != cur2)
+//	{
+//		cur1 = cur1->next;
+//		cur2 = cur2->next;
+//	}
+//	return cur1;
+//}
+//
+//SLLNode* BothLoop(SLLNode* head1, SLLNode* loop1, SLLNode* head2, SLLNode* loop2)
+//{
+//	SLLNode* cur1 = NULL;
+//	SLLNode* cur2 = NULL;
+//	if (loop1 == loop2)
+//	{
+//		cur1 = head1;
+//		cur2 = head2;
+//		int n = 0;
+//		while (cur1->next != loop1)
+//		{
+//			++n;
+//			cur1 = cur1->next;
+//		}
+//		while (cur2->next != loop2)
+//		{
+//			--n;
+//			cur2 = cur2->next;
+//		}
+//		cur1 = n > 0 ? head1 : head2;
+//		cur2 = cur1 == head1 ? head2 : head1;
+//		n = abs(n);
+//		while (n--)
+//		{
+//			cur1 = cur1->next;
+//		}
+//		while (cur1 != cur2)
+//		{
+//			cur1 = cur1->next;
+//			cur2 = cur2->next;
+//		}
+//		return cur1;
+//	}
+//	else
+//	{
+//		cur1 = loop1->next;
+//		while (cur1 != loop1)
+//		{
+//			if (cur1 == loop2)
+//			{
+//				return loop1;
+//			}
+//			cur1 = cur1->next;
+//		}
+//		return NULL;
+//	}
+//}
+//
+//SLLNode* GetIntersectionNode(SLLNode* head1, SLLNode* head2)
+//{
+//	if (head1 == NULL || head2 == NULL)
+//	{
+//		return NULL;
+//	}
+//	SLLNode* loop1 = GetLoopNode(head1);
+//	SLLNode* loop2 = GetLoopNode(head2);
+//	if (loop1 == NULL && loop2 == NULL)
+//	{
+//		return NoLoop(head1, head2);
+//	}
+//	if (loop1 != NULL && loop2 != NULL)
+//	{
+//		return BothLoop(head1, loop1, head2, loop2);
+//	}
+//	return NULL;
+//}
+//
+//void Test1()
+//{
+//	printf("******************************01******************************\n");
+//	SLLNode* head1;
+//	head1 = Create_SinglyLinkedListNode(1);
+//	head1->next = Create_SinglyLinkedListNode(2);
+//	head1->next->next = Create_SinglyLinkedListNode(3);
+//	head1->next->next->next = Create_SinglyLinkedListNode(4);
+//	head1->next->next->next->next = Create_SinglyLinkedListNode(5);
+//	head1->next->next->next->next->next = Create_SinglyLinkedListNode(6);
+//	head1->next->next->next->next->next->next = Create_SinglyLinkedListNode(7);
+//
+//	SLLNode* head2;
+//	head2 = Create_SinglyLinkedListNode(0);
+//	head2->next = Create_SinglyLinkedListNode(9);
+//	head2->next->next = Create_SinglyLinkedListNode(8);
+//
+//	head2->next->next->next = head1->next->next->next->next->next;
+//
+//	printf("%d\n", (GetIntersectionNode(head1, head2))->data);
+//	printf("*****************************EXIT*****************************\n\n");
+//}
+//
+//void Test2()
+//{
+//	printf("******************************02******************************\n");
+//	SLLNode* head1;
+//	head1 = Create_SinglyLinkedListNode(1);
+//	head1->next = Create_SinglyLinkedListNode(2);
+//	head1->next->next = Create_SinglyLinkedListNode(3);
+//	head1->next->next->next = Create_SinglyLinkedListNode(4);
+//	head1->next->next->next->next = Create_SinglyLinkedListNode(5);
+//	head1->next->next->next->next->next = Create_SinglyLinkedListNode(6);
+//	head1->next->next->next->next->next->next = Create_SinglyLinkedListNode(7);
+//	head1->next->next->next->next->next->next->next = head1->next->next->next;
+//
+//	SLLNode* head2;
+//	head2 = Create_SinglyLinkedListNode(0);
+//	head2->next = Create_SinglyLinkedListNode(9);
+//	head2->next->next = Create_SinglyLinkedListNode(8);
+//
+//	head2->next->next->next = head1->next;
+//
+//	printf("%d\n", (GetIntersectionNode(head1, head2))->data);
+//	printf("*****************************EXIT*****************************\n\n");
+//}
+//
+//void Test3()
+//{
+//	printf("******************************03******************************\n");
+//	SLLNode* head1;
+//	head1 = Create_SinglyLinkedListNode(1);
+//	head1->next = Create_SinglyLinkedListNode(2);
+//	head1->next->next = Create_SinglyLinkedListNode(3);
+//	head1->next->next->next = Create_SinglyLinkedListNode(4);
+//	head1->next->next->next->next = Create_SinglyLinkedListNode(5);
+//	head1->next->next->next->next->next = Create_SinglyLinkedListNode(6);
+//	head1->next->next->next->next->next->next = Create_SinglyLinkedListNode(7);
+//	head1->next->next->next->next->next->next->next = head1->next->next->next;
+//
+//	SLLNode* head2;
+//	head2 = Create_SinglyLinkedListNode(0);
+//	head2->next = Create_SinglyLinkedListNode(9);
+//	head2->next->next = Create_SinglyLinkedListNode(8);
+//
+//	head2->next->next->next = head1->next->next->next->next->next;
+//
+//	printf("%d\n", (GetIntersectionNode(head1, head2))->data);
+//	printf("*****************************EXIT*****************************\n\n");
+//}
+//
+//int main()
+//{
+//	Test1();
+//	Test2();
+//	Test3();
+//	return 0;
 //}
